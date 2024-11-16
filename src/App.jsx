e9019a4c06750a407f70347976d6aa3f781df2f0
hello <diogomcsilva5@gmail.com>
2024-11-16 18:14:38 +0000

    Finaly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 // App.jsx
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import RouterComponent from './RouterComponent';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './Layout/MainLayout';
+import Home from './Pages/Home';
+import Movies from './Pages/Movies';
+import Series from './Pages/Series';
+import Gender from './Pages/Gender';
+import Random from './Pages/Random';
+import SearchResults from './Pages/SearchResults';
+import Details from './Pages/Details';
 
 function App() {
   return (
     <Router basename="/S1lvaMovies">
-      <RouterComponent />
+      <Routes>
+        <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+        <Route path="/search" element={<MainLayout><SearchResults /></MainLayout>} />
+        <Route path="/movies" element={<MainLayout><Movies /></MainLayout>} />
+        <Route path="/series" element={<MainLayout><Series /></MainLayout>} />
+        <Route path="/gender" element={<MainLayout><Gender /></MainLayout>} />
+        <Route path="/random" element={<MainLayout><Random /></MainLayout>} />
+        <Route path="/movies/:id" element={<MainLayout><Details /></MainLayout>} />
+        <Route path="/series/:id" element={<MainLayout><Details /></MainLayout>} />
+        <Route path="/animes/:id" element={<MainLayout><Details /></MainLayout>} />
+      </Routes>
     </Router>
   );
 }
--- src/RouterComponent.jsx
+++ src/RouterComponent.jsx
@@ -1,46 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import MainLayout from './Layout/MainLayout';
-import Home from './Pages/Home';
-import Movies from './Pages/Movies';
-import Series from './Pages/Series';
-import Gender from './Pages/Gender';
-import Random from './Pages/Random';
-import SearchResults from './Pages/SearchResults';
-import Details from './Pages/Details';
-import LoadingScreen from './components/LoadingScreen';
-import usePrevious from './hooks/usePrevious';
+// // App.jsx
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import MainLayout from './Layout/MainLayout';
+// import Home from './Pages/Home';
+// import Movies from './Pages/Movies';
+// import Series from './Pages/Series';
+// import Gender from './Pages/Gender';
+// import Random from './Pages/Random';
+// import SearchResults from './Pages/SearchResults';
+// import Details from './Pages/Details';
 
-function RouterComponent() {
-    const [isLoading, setIsLoading] = useState(false);
-    const location = useLocation();
-    const prevLocation = usePrevious(location.pathname);
+// function App() {
+//   return (
+//     <Router basename="/S1lvaMovies">
+//       <Routes>
+//         <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+//         <Route path="/search" element={<MainLayout><SearchResults /></MainLayout>} />
+//         <Route path="/movies" element={<MainLayout><Movies /></MainLayout>} />
+//         <Route path="/series" element={<MainLayout><Series /></MainLayout>} />
+//         <Route path="/gender" element={<MainLayout><Gender /></MainLayout>} />
+//         <Route path="/random" element={<MainLayout><Random /></MainLayout>} />
+//         <Route path="/movies/:id" element={<MainLayout><Details /></MainLayout>} />
+//         <Route path="/series/:id" element={<MainLayout><Details /></MainLayout>} />
+//         <Route path="/animes/:id" element={<MainLayout><Details /></MainLayout>} />
+//       </Routes>
+//     </Router>
+//   );
+// }
 
-    useEffect(() => {
-        if (prevLocation !== location.pathname && location.pathname !== '/search') {
-            setIsLoading(true);
-            setTimeout(() => {
-                setIsLoading(false);
-            }, 2000);
-        }
-    }, [location.pathname, prevLocation]);
-
-    return (
-        <>
-        <Routes>
-  <Route path="/" element={<MainLayout><Home /></MainLayout>} />
-  <Route path="/search" element={<MainLayout><SearchResults /></MainLayout>} />
-  <Route path="/movies" element={<MainLayout><Movies /></MainLayout>} />
-  <Route path="/series" element={<MainLayout><Series /></MainLayout>} />
-  <Route path="/gender" element={<MainLayout><Gender /></MainLayout>} />
-  <Route path="/random" element={<MainLayout><Random /></MainLayout>} />
-  <Route path="/movies/:id" element={<MainLayout><Details /></MainLayout>} />
-  <Route path="/series/:id" element={<MainLayout><Details /></MainLayout>} />
-  <Route path="/animes/:id" element={<MainLayout><Details /></MainLayout>} />
-</Routes>
-
-        </>
-    );
-}
-
-export default RouterComponent;
+// export default App;
