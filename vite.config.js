7524bc35c5577344e7b8632a0affba5abfe5b7c5
hello <diogomcsilva5@gmail.com>
2024-11-16 06:17:01 +0000

    Finaly

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/S1lvaMovies/', // Substitua pelo nome do seu repositório
+  base: '/S1lvaMovies/', // Caminho base para o GitHub Pages
   plugins: [react()],
 });
